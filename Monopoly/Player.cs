91b1c30d5e993669dcd1d565081b30c5975144c9
Kevin Poston <kevposton@gmail.com>
2014-02-25 12:59:23 -0500

    Passed test for creating a player, and allowing the player to move around the board.

---
+++ Monopoly/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Monopoly
+{
+    public class Player
+    {
+        public Int32 Location { get; set; }
+
+        public Player()
+        {
+            Location = 0;
+        }
+
+        public void Rolled(Int32 rolled)
+        {
+            Location += rolled;
+
+            if (Location > 40)
+                Location -= 40;
+        }
+    }
+}
---
+++ MonopolyTests/PlayerTests.cs
@@ -0,0 +1,32 @@
+﻿using Monopoly;
+using NUnit.Framework;
+
+namespace MonopolyTests
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        private Player player;
+
+        [SetUp]
+        public void SetUp()
+        {
+            player = new Player();
+        }
+
+        [Test]
+        public void TestPlayerOnZeroRollsSevenAndMovesToSeven()
+        {
+            player.Rolled(7);
+            Assert.That(player.Location, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void TestPlayerOn39Rolls6AndEndsUpOn5()
+        {
+            player.Location = 39;
+            player.Rolled(6);
+            Assert.That(player.Location, Is.EqualTo(5));
+        }
+    }
+}
